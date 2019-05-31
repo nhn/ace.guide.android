d1d47aa5a469d9b3af9e2626a3855f155d188b97
유진상/데이터수집개발팀/AS <jinsang@nhnent.com>
2019-05-31 11:30:33 +0900

    불필요한 주석 제거

--- AceCounterSample/NativeApp/src/main/java/com/example/android/acecounter/MyApplication.java
+++ AceCounterSample/NativeApp/src/main/java/com/example/android/acecounter/MyApplication.java
@@ -10,7 +10,6 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         AceConfiguration aceConfiguration = new AceConfiguration.Builder(this)
-                //TODO : [필수] 보안을 위해 Resource ID를 사용하실 것을 권장해드립니다.
                 .setServiceId("100341")
                 //TODO : [선택] Activity의 PageView 자동 수집을 원하실 경우 setAutoPageView 를 true 로 설정 해주세요.
                 .setAutoPageView(true)
